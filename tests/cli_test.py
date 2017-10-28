6358a43428aceed748a66a3414a6cf95db8a9008
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-28 13:53:48 +0800

    Update test

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,4 +1,5 @@
 import io
+import os
 import sys
 import unittest
 from contextlib import redirect_stdout
@@ -15,6 +16,23 @@ class TestCli(unittest.TestCase):
 
         self.assertEqual(
             'Usage: wtouch file_name\n', f.getvalue())
+
+        sys.argv[1:] = ['test.js']
+        f = io.StringIO()
+
+        with redirect_stdout(f):
+            main()
+
+        file_name = 'test.js'
+        current_path = os.path.join(os.getcwd(), '.')
+
+        self.assertTrue(os.path.exists(os.path.join(current_path, file_name)))
+
+    def tearDown(self):
+        file_name = 'test.js'
+        current_path = os.path.join(os.getcwd(), '.')
+
+        os.remove(os.path.join(current_path, file_name))
 
 if __name__ == '__main__':
     unittest.main()
